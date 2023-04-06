e0dacdbefe2ae5b344ec872110f06cbbbb2cace1
hctr03 <94515098+hctr03@users.noreply.github.com>
2023-04-06 17:52:33 -0600

    Encontrar un determinado numero en un Array.
    Para el siguiente array, crear el proceso necesario para solicitar
    al usuario ingresar un número, luego buscar ese número en el
    array y por último imprimir si se logró encontrar o no el número.
    Preguntar al final si desea ingresar otro número.
    • int[10]test={ 32, 87, 3, 45, 76, 34, 65, 85, 74, 93},

---
+++ Encontrar Numero/Encontrar Numero/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int[] test = { 32, 87, 3, 45, 76, 34, 65, 85, 74, 93 };
+int opcion = 0;
+bool encontrado = false;
+
+do
+{
+    Console.WriteLine("Ingrese un numero para ver si lo tenemos en nuestro array");
+    int num = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < test.Length; i++)
+    {
+        if (test[i] == num)
+        {
+            encontrado = true;
+        }
+        
+    }
+    if (encontrado == true)
+    {
+        Console.WriteLine("El valor se encuentra");
+        encontrado = false;
+    }
+    else
+    { 
+        Console.WriteLine("El valor no se encuentra");
+    }
+    Console.WriteLine("Quiere intentarlo otra vez?\n1- Si\n2- No");
+    opcion = int.Parse(Console.ReadLine()); 
+} while (opcion != 2);
